ab7840304bf1ce4885ebe44218e4fb8ab5f74e30
olga rachkova <olgarachkova96@gmail.com>
2022-03-02 14:39:26 +0700

    region component edit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import bagansky from './bagansky.json';
 import Region from './Region';
 import suzunsky from './suzunsky.json';
 
-/*function latlngToPx(mapCurrentX, mapCurrentY, viewboxStartX, viewboxStartY, viewboxEndX, viewboxEndY, mapStartX, mapStartY, mapEndX, mapEndY) {
-  const coefX = (viewboxEndX - viewboxStartX) / (mapEndX - mapStartX);
-  const coefY = (viewboxEndY - viewboxStartY) / (mapEndY - mapStartY);
-  const viewboxCurrentX = (mapCurrentX - mapStartX) * coefX;
-  const viewboxCurrentY = (mapCurrentY - mapStartY) * coefY;
-  return [viewboxCurrentY, viewboxCurrentX];
-}*/
-
 function App() {
 
   const viewboxStartX = 0;
@@ -26,20 +18,23 @@ function App() {
   const mapEndX = 53.189318;
   const mapEndY = 89.591162;
 
-  /*const baganskyToSVG = bagansky[0].geojson.coordinates[0].map(item => {
-    return latlngToPx(item[1], item[0], viewboxStartX, viewboxStartY, viewboxEndX, viewboxEndY, mapStartX, mapStartY, mapEndX, mapEndY);
-  });
-  const suzunskyToSVG = suzunsky[0].geojson.coordinates[0].map(item => {
-    return latlngToPx(item[1], item[0], viewboxStartX, viewboxStartY, viewboxEndX, viewboxEndY, mapStartX, mapStartY, mapEndX, mapEndY);
-  });*/
-
   const [info, setInfo] = useState('');
 
   return (
     <div className="App">
       <svg viewBox={`${viewboxStartX} ${viewboxStartY} ${viewboxEndX} ${viewboxEndY}`} xmlns="http://www.w3.org/2000/svg">
-        <Region geo={bagansky} setInfo={setInfo} />
-        <Region geo={suzunsky} setInfo={setInfo} />
+        <Region
+          geo={bagansky}
+          viewbox={[viewboxStartX, viewboxStartY, viewboxEndX, viewboxEndY]}
+          map={[mapStartX, mapStartY, mapEndX, mapEndY]}
+          setInfo={setInfo}
+        />
+        <Region
+          geo={suzunsky}
+          viewbox={[viewboxStartX, viewboxStartY, viewboxEndX, viewboxEndY]}
+          map={[mapStartX, mapStartY, mapEndX, mapEndY]}
+          setInfo={setInfo}
+        />
       </svg>
       <div className='infoblock'>{info}</div>
     </div>
--- src/Region.js
+++ src/Region.js
@@ -10,11 +10,11 @@ function latlngToPx(mapCurrentX, mapCurrentY, viewboxStartX, viewboxStartY, view
 }
 
 
-const Region = ({ geo, setInfo }) => {
+const Region = ({ geo, viewbox, map, setInfo }) => {
 
     const [fillColor, setFillColor] = useState('transparent');
 
-    const viewboxStartX = 0;
+    /*const viewboxStartX = 0;
     const viewboxStartY = 0;
     const viewboxEndX = 400;
     const viewboxEndY = 700;
@@ -22,10 +22,10 @@ const Region = ({ geo, setInfo }) => {
     const mapStartX = 57.551841;
     const mapStartY = 75.273074;
     const mapEndX = 53.189318;
-    const mapEndY = 89.591162;
+    const mapEndY = 89.591162;*/
 
     const geoPoints = geo[0].geojson.coordinates[0].map(item => {
-        return latlngToPx(item[1], item[0], viewboxStartX, viewboxStartY, viewboxEndX, viewboxEndY, mapStartX, mapStartY, mapEndX, mapEndY);
+        return latlngToPx(item[1], item[0], ...viewbox, ...map);
     }).join(' ');
 
     const geoInfo = geo[0].display_name;
